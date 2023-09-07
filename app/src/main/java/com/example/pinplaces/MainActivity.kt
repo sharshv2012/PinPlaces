aae14d127e1f5ad78f5702ccb8237e32a9a1c0bf
Harsh <sharshvardhan759@gmail.com>
2023-09-07 16:27:01 +0530

    1st commit

---
+++ app/src/main/java/com/example/pinplaces/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pinplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
