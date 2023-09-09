5e4103c0153cd6999738b52015d304cdaa7c2ee1
Harsh <sharshvardhan759@gmail.com>
2023-09-10 00:00:07 +0530

    3rd commit: worked on add place activity.

--- app/src/main/java/com/example/pinplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/pinplaces/AddHappyPlaceActivity.kt
@@ -2,11 +2,13 @@ package com.example.pinplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.pinplaces.databinding.ActivityAddHappyPlaceBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class AddHappyPlaceActivity : AppCompatActivity() {
-    var binding:ActivityAddHappyPlaceBinding? = null
+    private var binding:ActivityAddHappyPlaceBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
@@ -14,22 +16,34 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         setSupportActionBar(binding?.toolbarPinPlace)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         binding?.toolbarPinPlace?.setNavigationOnClickListener {
-
+            alertDialogFunction()
         }
     }
 
-    override fun onBackPressed() {
-        //showing dialog and then closing the application..
-        showDialog()
-    }
+   // no need of onBackPressed()
 
-    private fun showDialog(){
-        MaterialAlertDialogBuilder(this).apply {
-            setTitle("are you sure?")
-            setMessage("want to close the application ?")
-            setPositiveButton("Yes") { _, _ -> finish() }
-            setNegativeButton("No", null)
-            show()
+    private fun alertDialogFunction(){
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Alert")
+        builder.setMessage("this is an alert dialog. which is used to show alert in our app.")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+        builder.setPositiveButton("yes") {dialogInterface ,
+                                          which -> finish()
+            dialogInterface.dismiss()
         }
+
+        builder.setNeutralButton("cancel"){ dialogInterface , which->
+            dialogInterface.dismiss()
+        }
+        builder.setNegativeButton("No"){ dialogInterface , which->
+            dialogInterface.dismiss()
+        }
+
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false) // will not allow user to cancel by clicking on the remaining area
+        alertDialog.show()
+
     }
 }
