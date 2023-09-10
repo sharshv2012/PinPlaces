236db41b3fd930b58431647d093409646e88ab94
Harsh <sharshvardhan759@gmail.com>
2023-09-10 15:44:21 +0530

    4th commit: Added DatePicker and set that date we got from datePicker into editText.

--- app/src/main/java/com/example/pinplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/pinplaces/AddHappyPlaceActivity.kt
@@ -1,14 +1,24 @@
 package com.example.pinplaces
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.pinplaces.databinding.ActivityAddHappyPlaceBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
     private var binding:ActivityAddHappyPlaceBinding? = null
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
@@ -18,10 +28,22 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         binding?.toolbarPinPlace?.setNavigationOnClickListener {
             alertDialogFunction()
         }
+
+        dateSetListener = OnDateSetListener {
+                view, year, month, dayOfMonth ->
+
+            cal.set(Calendar.YEAR , year)
+            cal.set(Calendar.MONTH , month)
+            cal.set(Calendar.DAY_OF_MONTH , dayOfMonth)
+            updateDateInView()
+        }
+        binding?.etDate?.setOnClickListener(this)
     }
 
-   // no need of onBackPressed()
 
+    override fun onBackPressed() {
+        alertDialogFunction()
+    }
     private fun alertDialogFunction(){
 
         val builder = AlertDialog.Builder(this)
@@ -45,5 +67,25 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         alertDialog.setCancelable(false) // will not allow user to cancel by clicking on the remaining area
         alertDialog.show()
 
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!){
+            binding?.etDate ->{
+                DatePickerDialog(this@AddHappyPlaceActivity ,
+                    dateSetListener ,
+                    cal.get(Calendar.YEAR) ,
+                    cal.get(Calendar.MONTH) ,
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+
+            }
+        }
+    }
+
+    private fun updateDateInView(){
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat , Locale.getDefault())
+        binding?.etDate!!.setText(sdf.format(cal.time).toString())
     }
 }
